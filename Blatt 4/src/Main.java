1c0305dc95aa838e6de9f7b4d3c5ede22e41d688
Sascha Graeff <sascha@blackicegames.de>
2013-11-07 19:54:18 +0100

    Aufgabe 2.

--- Blatt 4/src/Main.java
+++ Blatt 4/src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main
 {
-    public static final boolean debug = true;
+    public static final boolean debug = false;
 
     public static void main(String[] args)
     {
-        Labyrinth labyrinth = new Labyrinth(LabyrinthDataLoader.load("data.txt"));
+        Labyrinth labyrinth = new Labyrinth(LabyrinthDataLoader.load("data_blocked.txt"));
         
         System.out.println(new LabyrinthPathfinderAStar(labyrinth).findPath() != null);
         System.out.println(labyrinth);
