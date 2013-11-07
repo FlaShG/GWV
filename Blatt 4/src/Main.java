e94ba3d313aec8622b427d49b928be6f7818261c
Sascha Graeff <sascha@blackicegames.de>
2013-11-07 20:26:23 +0100

    Finishing.

--- Blatt 4/src/Main.java
+++ Blatt 4/src/Main.java
@@ -5,9 +5,22 @@ public class Main
 
     public static void main(String[] args)
     {
-        Labyrinth labyrinth = new Labyrinth(LabyrinthDataLoader.load("data_teleport.txt"));
+        pathfindingOn("data.txt");
+        pathfindingOn("data_blocked.txt");
+        pathfindingOn("data_teleport.txt");
+    }
+    
+    private static void pathfindingOn(String filename)
+    {
+        Labyrinth labyrinth = new Labyrinth(LabyrinthDataLoader.load(filename));
+        
+        long startTime = System.currentTimeMillis();
         
         System.out.println(new LabyrinthPathfinderAStar(labyrinth).findPath() != null);
+        
+        
+        long delta = System.currentTimeMillis() - startTime;
         System.out.println(labyrinth);
+        System.out.println("...and I needed "+(delta / 1000f)+" Seconds for that!");
     }
 }
