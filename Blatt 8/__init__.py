d593a50a4680c03d0a58753cc8fe6b9f788c15f5
Sascha Graeff <sascha@blackicegames.de>
2013-12-05 15:26:51 +0100

    awesome init

--- Blatt 8/__init__.py
+++ Blatt 8/__init__.py
@@ -1,13 +1,22 @@
-       
+import codecs
+import re
+    
 
 def get_file_contents(filename):
-    f = open(filename, 'r')
+    #f = open(filename, 'r')
+    f = codecs.open(filename, encoding='utf-8') 
     lines = []
     for line in f:
-        lines.append(line)
+        #remove \r\n
+        nice_line = re.sub("\r\n", lambda x: "", line)
+        #only add if long enough (not a symbol like . or , or ")
+        if len(nice_line) > 1:
+            lines.append(nice_line)
     f.close()
     return lines
     
 
 heise_lines = get_file_contents('heise.txt')
 print(heise_lines)
+
+#dictionary = dict()
