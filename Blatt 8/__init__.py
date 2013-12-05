9f386f79c959d2188159f03b13c47b6cdd0e078e
Sascha Graeff <sascha@blackicegames.de>
2013-12-05 15:08:21 +0100

    Noch n Merge und python foo

--- Blatt 8/__init__.py
+++ Blatt 8/__init__.py
@@ -1,2 +1,13 @@
+       
 
-print "hello world"
+def get_file_contents(filename):
+    f = open(filename, 'r')
+    lines = []
+    for line in f:
+        lines.append(line)
+    f.close()
+    return lines
+    
+
+heise_lines = get_file_contents('heise.txt')
+print(heise_lines)
