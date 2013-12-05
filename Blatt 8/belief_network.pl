d27216639b7aa62e21ddfc165f9ddd18e4b931c1
Sascha Graeff <sascha@blackicegames.de>
2013-12-05 17:12:08 +0100

    belief-Zeug.

--- Blatt 8/belief_network.pl
+++ Blatt 8/belief_network.pl
@@ -1,9 +1,10 @@
 
-battery_w       :- battery_ok
-ignition_key_w  :- ignition_key_ok
-efl_w           :- efl_ok * battery_w
-starter_w       :- starter_ok * ignition_key_w
-engine_w        :- engine_ok * starter_w * filter_w
-filter_w        :- filter_ok * fuel_pump_w
-filter_pump_w   :- fuel_pump_ok * efl_w * fuel_tank_w
-fuel_tank_w     :- fuel_tank_ok
+
+battery_w       :- battery_ok(0.9)
+ignition_key_w  :- ignition_key_ok(0.9)
+efl_w           :- efl_ok(0.9) * battery_w
+starter_w       :- starter_ok(0.9) * ignition_key_w
+engine_w        :- engine_ok(0.9) * starter_w * filter_w
+filter_w        :- filter_ok(0.9) * fuel_pump_w
+filter_pump_w   :- fuel_pump_ok(0.9) * efl_w * fuel_tank_w
+fuel_tank_w     :- fuel_tank_ok(0.9)
